248c7705993f7037180ce4c2b25e1231dcf49069
SekllanChenRuan <xirenchen94@gmail.com>
2019-04-11 11:04:00 -0700

    setup of first view
    setup first view and base.html and index.html

--- TechReviewProj/TechReviewProj/urls.py
+++ TechReviewProj/TechReviewProj/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('TechReivewApp/', include('TechReviewApp.urls')),
+    path('TechReviewApp/', include('TechReviewApp.urls')),
 ]
