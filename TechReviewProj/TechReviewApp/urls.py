2aa3b9b09dd91689938a5be9e7a4f9f1b66d89cb
SekllanChenRuan <xirenchen94@gmail.com>
2019-05-02 11:24:53 -0700

    added a product view and product detail view

--- TechReviewProj/TechReviewApp/urls.py
+++ TechReviewProj/TechReviewApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.index, name='index'),  
     path('getTypes/', views.getTypes, name='types'),
+    path('getProducts/', views.getProducts, name='products'),
+    path('productDetails/<int:id>', views.productDetails, name='productdetails'),
 ]
--- TechReviewProj/TechReviewApp/views.py
+++ TechReviewProj/TechReviewApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import TechType, Product, Review
 
 # Create your views here.
@@ -8,4 +8,19 @@ def index(request):
 def getTypes(request):
     types_list=TechType.objects.all()
     return render(request, 'TechReviewApp/types.html',{'types_list' : types_list})
+
+def getProducts(request):
+    products_list=Product.objects.all()
+    return render(request, 'TechReviewApp/products.html',{'products_list': products_list})
+
+def productDetails(request, id):
+    prod=get_object_or_404(Product, pk=id)
+    reviewcount=Review.objects.filter(product=id).count()
+    reviews=Review.objects.filter(product=id)
+    context={
+        'prod' : prod,
+        'reviewcount' : reviewcount,
+        'reviews' : reviews,
+    }
+    return render(request, 'TechReviewApp/proddetails.html', context=context)
 
